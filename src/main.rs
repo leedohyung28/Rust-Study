cfffa145541c25a87e85f60c6e38fb8750218407
leedohyung28 <dhleee0123@gmail.com>
2023-09-04 11:54:47 +0900

    Step 1-3

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     // step1_1();
-    step1_2();
+    // step1_2();
+    step1_3();
 }
 
 fn step1_1() {
@@ -37,7 +38,7 @@ fn brackets_test() {
 fn step1_2() {
     // let_test();
     // mut_test();
-    variable_shadowing_test();
+    // variable_shadowing_test();
 }
 fn let_test() {
     let a_number;
@@ -65,3 +66,60 @@ fn variable_shadowing_test() {
     // 기존 변수의 이름을 활용하는 새 변수를 선언 가능하다.
     // 이를 Rust에서는 "Shadowing"이라고 한다.
 }
+
+fn step1_3() {
+    // num_type_test();
+    // float_type_test();
+    // bool_test();
+    // char_test();
+
+    // Rust에서 char는 알파벳, 숫자, 이미지 등을 비롯한 모든 단일 문자
+}
+fn num_type_test() {
+    let number: u32 = 14;
+    println!("The number is {}.", number);
+
+    // 길이   : 부호O,  부호X
+    // 8비트  :    i8,    u8
+    // 16비트 :   i16,   u16
+    // 32비트 :   i32,   u32
+    // 64비트 :   i64,   u64
+    // 128비트:  i128,  u128
+    // 종속   : isize, usize
+}
+fn float_type_test() {
+    // 10지수 : f32, f64 졵 (기본 부동 소수점 형식 : f64)
+    // f64는 f32와 속도는 비슷 but 더욱 정밀
+
+    let number_64 = 4.0;    // compiler가 자동적으로 f64로 인식
+    let number_32: f32 = 5.0;    // f32가 되도록 명시적 선언
+
+    println!("1+2 = {} and 8-5 = {} and 15*3 = {}", 1u32 + 2, 8i32 - 5 , 15 * 3);
+    println!("9/2 = {} but 9.0/2.0 = {}", 9u32 / 2, 9.0/2.0);
+    // 정수와 FLoating point의 나눗셈 차이
+}
+fn bool_test() {
+    let is_bigger = 1 > 4;
+    println!("Is 1 > 4? {}", is_bigger);
+    // false라고 출력
+
+    let is_smaller = 1 < 4;
+    println!("Is 1 < 4? {}", is_smaller);
+}
+fn char_test() {
+    let uppercase_s = 'S';
+    let lowercase_f = 'f';
+    let smiley_face = '😃';
+    // 모든 텍스트 형식은 유효한 UTF-8 표현
+    // char는 너비가 32비트가 되도록 채워지는 21비트 정수.
+    // char는 일반 코드 포인트 값을 직접 포함
+
+    let string_1 = "miley";
+    let string_2 = "ace";
+    // 'str'형식은 문자열 조각으로 '&str'가 있는 형식 앞의 참조 스타일 구문 사용
+    // String : 프로그램이 실행될 때 변경될 수 있는 텍스트 데이터
+    // &str : 프로그램이 실행될 때 변하지 않는 텍스트 데이터에 대한 변경 불가능한 보기
+
+    println!("{} is a {}{}{}{}.", smiley_face, uppercase_s, string_1, lowercase_f, string_2);
+}
+
